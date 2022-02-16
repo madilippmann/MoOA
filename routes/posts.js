f316d71c5f2b4e862db316e86a7f2bc647cd67d1
Madi Lippmann <madi.lippmann@gmail.com>
2022-02-16 08:59:03 -0800

    POST /posts fully functional

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { csrfProtection, asyncHandler, userValidators, loginValidators, validationResult } = require('./utils')
+const { csrfProtection, asyncHandler, validationResult, postValidator } = require('./utils')
 const db = require('../db/models');
 const { requireAuth } = require('../auth')
 
@@ -12,7 +12,39 @@ router.get('/create', requireAuth, csrfProtection, (req, res, next) => {
     })
 })
 
-router.post('/', )
+
+router.post('/', postValidator, requireAuth, csrfProtection, asyncHandler(async (req, res, next) => {
+
+    const { title, imageURL, description } = req.body;
+
+    const userId = req.session.auth.userId
+
+    console.log("USER ID: ", userId)
+
+    const post = await db.Post.build({
+        title,
+        user_id: userId,
+        path: imageURL,
+        description
+    });
+
+    const validatorErrors = validationResult(req);
+
+    if (validatorErrors.isEmpty()) {
+        await post.save();
+
+        res.redirect(`/posts/${post.id}`)
+    } else {
+        const errors = validatorErrors.array().map(error => error.msg);
+
+        res.render('create-post', {
+            csrfToken: req.csrfToken(),
+            title: "Create New Post",
+            post,
+            errors
+        })
+    }
+}))
 
 
 module.exports = router;
