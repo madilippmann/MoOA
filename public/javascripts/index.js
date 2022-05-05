9a01a813f8e6161d2984a26c0dbb8436a7b96219
Madi Lippmann <madi.lippmann@gmail.com>
2022-05-05 13:18:09 -0700

    Fixed user liking their own post bug and fixed follow button console error message

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,55 +1,16 @@
-window.addEventListener("DOMContentLoaded", (event)=>{
-
+window.addEventListener("DOMContentLoaded", (event) => {
     const isEmpty = obj => Object.keys(obj).length === 0;
 
-    const followButton = document.querySelector(".follow-icon");
-
-    followButton.addEventListener('click', async() => {
-
-        const handleLink = document.getElementById('handle-username');
-        const followsCount = document.querySelector('.follows-count')
-        const url = handleLink.getAttribute('href');
-        const username = url.split('/')[1];
-
-        try {
-            const res = await fetch('/follows', {
-                method: "PUT",
-                body: JSON.stringify({username}),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-
-            if (!res.ok) {
-                throw res;
-            }
-
-            const newFollow = await res.json();
-
-            if (!isEmpty(newFollow)) {
-                followsCount.innerHTML = Number(followsCount.innerHTML) + 1
-                followButton.classList.add('followed')
-            } else {
-                followsCount.innerHTML = Number(followsCount.innerHTML) - 1
-                followButton.classList.remove('followed')
-            }
-
-        } catch (err){}
-
-    })
-
     const likeButton = document.getElementById('like-button');
-
     likeButton.addEventListener('click', async () => {
 
         const postUrl = window.location.href.split('/');
         const postId = postUrl[postUrl.length - 1];
         const likesCount = document.querySelector('.likes-num')
-
         try {
             const res = await fetch('/likes', {
                 method: "PUT",
-                body: JSON.stringify({postId}),
+                body: JSON.stringify({ postId }),
                 headers: {
                     'Content-Type': 'application/json'
                 }
@@ -68,6 +29,47 @@ window.addEventListener("DOMContentLoaded", (event)=>{
                 likesCount.innerHTML = Number(likesCount.innerHTML) - 1
                 likeButton.classList.remove('liked')
             }
-        } catch (err) {}
+        } catch (err) { }
     })
+
+
+    const followButton = document.querySelector(".follow-icon");
+    if (followButton) {
+        followButton.addEventListener('click', async () => {
+
+            const handleLink = document.getElementById('handle-username');
+            const followsCount = document.querySelector('.follows-count')
+            const url = handleLink.getAttribute('href');
+            const username = url.split('/')[1];
+
+            try {
+                const res = await fetch('/follows', {
+                    method: "PUT",
+                    body: JSON.stringify({ username }),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+
+                if (!res.ok) {
+                    throw res;
+                }
+
+                const newFollow = await res.json();
+
+                if (!isEmpty(newFollow)) {
+                    followsCount.innerHTML = Number(followsCount.innerHTML) + 1
+                    followButton.classList.add('followed')
+                } else {
+                    followsCount.innerHTML = Number(followsCount.innerHTML) - 1
+                    followButton.classList.remove('followed')
+                }
+
+            } catch (err) { }
+
+        })
+
+    }
+
+
 })
